ba379ff51ceff7b8ae87ccf1c104320ecc35b0f5
fashionz <esubafashionz@gmail.com>
2020-05-22 16:19:56 +0200

    06. Ahora la feed sale ordenada cronológicamente. Editada la clase Muro y MuroTest

--- src/main/java/Muro.java
+++ src/main/java/Muro.java
@@ -8,47 +8,24 @@ import java.util.ArrayList;
  * @author DAM
  * @version 1.0
  */
-public class Muro
-{
-    // Entradas de texto
-    private ArrayList <EntradaTexto> mensajes;
-    // Entradas con imagenes
-    private ArrayList <EntradaFoto> fotos;
-    // Entradas con eventos
-    private ArrayList<EntradaEvento> eventos;
+public class Muro {
+
+    private ArrayList<Entrada> entradas;
 
     /**
      * Constructor - Construye e inicializa un muro vacio.
      */
     public Muro() {
         //Inicializa mensajes y fotos.
-        mensajes = new ArrayList<>();
-        fotos = new ArrayList<>();
-        eventos = new ArrayList<>();
+        entradas = new ArrayList<>();
     }
 
     /**
-     * Anade una entrada de texto al muro.
-     * @param entrada La entrada de texto a anadir.
+     * Anade una entrada de evento al muro.
+     * @param entrada La entrada de evento a anadir.
      */
-    public void addEntradaTexto(EntradaTexto entrada) {
-        mensajes.add(entrada);
-    }
-
-    /**
-     * Anade una entrada de imagen al muro.
-     * @param entradaFoto La entrada de imagen a anadir.
-     */
-    public void addEntradaFoto(EntradaFoto entradaFoto) {
-        fotos.add(entradaFoto);
-    }
-
-    /**
-     * Anade una entrada de imagen al muro.
-     * @param entradaEvento La entrada de imagen a anadir.
-     */
-    public void addEntradaEvento(EntradaEvento entradaEvento) {
-        eventos.add(entradaEvento);
+    public void addEntrada(Entrada entrada) {
+        entradas.add(entrada);
     }
 
     /**
@@ -58,22 +35,9 @@ public class Muro
     @Override
     public String toString() {
         String aDevolver = "";
-
-        // Buscamos todas las entradas de texto.
-        for (EntradaTexto entrada : mensajes) {
-            aDevolver += entrada + "\n\n";
-        }
-
-        // Buscamos todas las entradas con foto.
-        for (EntradaFoto entrada : fotos) {
-            aDevolver += entrada + "\n\n";
-        }
-
-        // Buscamos todas las entradas con evento.
-        for (EntradaEvento entrada : eventos) {
+        for (Entrada entrada : entradas) {
             aDevolver += entrada + "\n\n";
         }
         return aDevolver;
     }
 }
-
--- src/test/java/MuroTest.java
+++ src/test/java/MuroTest.java
@@ -22,16 +22,16 @@ public class MuroTest {
         EntradaEvento entrada05 = new EntradaEvento("Roberto", "Se ha unido a la red");
         EntradaEvento entrada06 = new EntradaEvento("Christian", "Ha hecho una tortilla de patata");
 
-        muro.addEntradaTexto(entrada01);
-        muro.addEntradaFoto(entrada02);
-        muro.addEntradaTexto(entrada03);
-        muro.addEntradaFoto(entrada04);
-        muro.addEntradaEvento(entrada05);
-        muro.addEntradaEvento(entrada06);
+        muro.addEntrada(entrada01);
+        muro.addEntrada(entrada02);
+        muro.addEntrada(entrada03);
+        muro.addEntrada(entrada04);
+        muro.addEntrada(entrada05);
+        muro.addEntrada(entrada06);
 
         String comparacion = "Usuario: Nacho\nLikes: 0\nRecién llegado de mis vacaciones en Hawai!\nEscrito hace 10 segundos\nNo hay comentarios\n\n\n" +
-                "Usuario: Begoña\nLikes: 0\nEstá lloviendo ahí fuera...\nEscrito hace 10 segundos\nNo hay comentarios\n\n\n" +
                 "Usuario: Luis\nLikes: 0\nUrl: https://bit.ly/2W1dO09\nTitulo: Gracias\nEscrito hace 10 segundos\nNo hay comentarios\n\n\n" +
+                "Usuario: Begoña\nLikes: 0\nEstá lloviendo ahí fuera...\nEscrito hace 10 segundos\nNo hay comentarios\n\n\n" +
                 "Usuario: Cristina\nLikes: 0\nUrl: https://bit.ly/3cY9vtD\nTitulo: Una foto de mi nevera\nEscrito hace 10 segundos\nNo hay comentarios\n\n\n" +
                 "Usuario: Roberto\nLikes: 0\nSe ha unido a la red\nEscrito hace 10 segundos\n\n\n" +
                 "Usuario: Christian\nLikes: 0\nHa hecho una tortilla de patata\nEscrito hace 10 segundos\n\n\n";
